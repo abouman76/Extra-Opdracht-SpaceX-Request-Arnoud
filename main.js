987ee49caa4fe7bba3b148860d87fdbc74bd5161
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-02-09 13:58:03 +0100

    opdracht klaar

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
 //          - [ ] li appenden.
 
 async function dataSpaceX() {
-    console.log("Wat log ik?")
+    // console.log("Wat log ik?")
     const responseFromApi = await axios.get(
         "https://api.spacexdata.com/v4/launches"
     );
@@ -26,15 +26,21 @@ async function dataSpaceX() {
    // console.log("Info SpaceX Sat:", responseFromApi.data[3].name, responseFromApi.data[3].details);
 
     const listOfSatellites = document.getElementById("satellite-list");
-    console.log(listOfSatellites);
+    // console.log(listOfSatellites);
 
-    console.log(responseFromApi.data);
+    // console.log(responseFromApi.data); data.details geeft nog iterable aan!!! zie hieronder.
 
     for (const launch of responseFromApi.data) {
         // console.log("1 tegelijk?", launch);
         const launchSatellite = `${launch.name} - ${launch.details}`;
         console.log(launchSatellite);
+        // .name en .details zijn geen direct onderdeel van .data. Daarom roep je ze via
+        // launch.date en launch.details aan in een string literal.
+        const satelliteInfo = document.createElement("li");
+        satelliteInfo.textContent = launchSatellite;
+        // console.log("Info Satellite:", satelliteInfo);
 
+        listOfSatellites.appendChild(satelliteInfo);
     }
 
 }
