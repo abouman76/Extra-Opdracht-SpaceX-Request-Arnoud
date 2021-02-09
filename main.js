bb7427d8d743a677d16331061dc79145ba34f066
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-02-09 13:09:50 +0100

    made request and async await

---
+++ main.js
@@ -0,0 +1,20 @@
+// [x] HTML en JS files maken.
+// [x] JavaScript file linken.
+// [x] NPM init.
+// [x] git init (zorg wel voor een git repository)
+// [x] .gitignore maken.
+// [x] AXIOS installeren. -> npm install axios _(library)_
+// [x] AXIOS script linken.
+// [ ] Een async function maken en **aanroepen**!
+// [ ] Request maken met AXIOS.
+// [ ] Afwachten tot de data er is.
+// [ ] Data weergeven in de DOM.
+//     - [ ] Lijst maken met een id.
+//     - [ ] Lijst selecteren.
+//     - [ ] Voor elke element in de array.
+//          - [] Een li aanmaken.
+//          - [ ] textContent toevoegen.
+//          - [ ] li appenden.
+
+
+
